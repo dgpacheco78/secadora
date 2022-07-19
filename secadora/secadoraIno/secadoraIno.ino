7481d515d2701ab671b3617093f7eeb634c3b7b0
dgpacheco78 <96139278+dgpacheco78@users.noreply.github.com>
2022-07-19 13:06:59 -0500

    Update secadoraIno.ino

--- secadora/secadoraIno/secadoraIno.ino
+++ secadora/secadoraIno/secadoraIno.ino
@@ -1,3 +1,17 @@
+/*
+Secadora Industrial de Alimentos
+David García Pacheco
+Alfonso Monterrosas Fuentes
+Salomón Tapia Aguilar
+Universidad Tecnológica de Izúcar de Matamoros
+
+Control de sensores y adquisición de datos de los sensores:
+- Sensor de temperatura MAX6675 (2), leer la temperatura en diferentes partes del horno
+- Sensor de carga, leer la perdida de peso de las muestras durante el proceso de secado
+- Sensor de efecto hall, para determinas las revoluciones por minuto del ventilador
+- Servomotores (2), controla la perilla de la temperatura del horno y la perilla para controlar la velocidad del ventilador
+*/
+
 #include <max6675.h>                  //librerias necesarias para el
 #include <LiquidCrystal_I2C.h>        //funcionamiento del sensor de temperatura MAX6675,
 #include "HX711.h"                    //display 16x2 con I2C, sensor de carga HX711 y
@@ -7,6 +21,7 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 MAX6675 thermo1(5, 6, 7);
 MAX6675 thermo2(8, 9, 10);
+Servo servoTemp, servoVelo;
 HX711 balanza;
 float carga, temp2, tempProm;
 const int DOUT=A1;
@@ -15,6 +30,39 @@ String datoPython = "";
 int estado = 0;
 int rpm = 0;
 
+/*
+Conexiones
+Arduino uno   HX711
+A0            SCK
+A1            DT
+Vcc           Vcc
+Gnd           Gnd
+
+              I2C (LCD)
+A4            SDA
+A5            SCL
+Vcc           Vcc
+Gnd           Gnd
+
+              KY-024 (Sensor efecto hall)
+D4            D0
+Vcc           Vcc
+Gnd           Gnd
+
+              MAX6675 (1)
+D5            SCK
+D6            CS
+D7            SO
+Vcc           Vcc
+Gnd           Gnd
+
+              MAX6675 (2)
+D8            SCK
+D9            CS
+D10           SO
+Vcc           Vcc
+Gnd           Gnd
+*/
 
 void setup() {                //inicialización de objetos y variables
   lcd.init();
@@ -28,6 +76,9 @@ void setup() {                //inicialización de objetos y variables
   balanza.set_scale(-180);    //inicialización y calibración del sensor de carga
   balanza.tare(5000);         //estableciendo tare, para nuestro caso 5000 (5 Kg)
   Serial.begin(115200);       //estableciendo puerto para envio y recepción de datos
+  servoTemp.attach(11);       //se coloca a la mitad ya que, de acuerdo a las pruebas,
+  servoVelo.attach(12);       //es la posicion para la temperatura de operación: 70°C
+  servoTemp.write(90);        
 }
 
 void loop() {
@@ -45,7 +96,7 @@ void loop() {
     if(datoPython.equals("p")){         //si el dato de entrada es "p" inicia el proceso de precalentado del horno
       estado = 1;
     }
-    if(datoPython.equals("i")){         ////si el dato de entrada es "i" inicia el proceso de secado de alimentos
+    if(datoPython.equals("i")){         //si el dato de entrada es "i" inicia el proceso de secado de alimentos
       estado = 2;
     }
     delay(500);
@@ -96,16 +147,16 @@ void loop() {
     if(Serial.available() > 0) {
       datoPython = Serial.readString();
       datoPython.trim();
-      if(datoPython.equals("f")){
-        estado = 0;
+      if(datoPython.equals("f")){           //durante el proceso de secado, si se recibe una "f" indica que el ciclo
+        estado = 0;                         //de secado a terminado y el proceso de secado finaliza apagando el horno
         lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print("Fin de ciclo");
         delay(2000);
         break;
       }
-      if(datoPython.equals("e")){
-        estado = 0;
+      if(datoPython.equals("e")){           //"e" significa paro de emergencia, en caso de que se
+        estado = 0;                         //se necesite que el sistema se detenga
         lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print("Paro de emergencia");
